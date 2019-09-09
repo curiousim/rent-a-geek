71aa9f5f6b8f99852e1989958da803a734e6ee76
IgorZ <igor_zlobinski@bezeqint.net>
2019-09-09 12:20:05 +0300

    seed and new geek route view added

--- app/controllers/geeks_controller.rb
+++ app/controllers/geeks_controller.rb
@@ -1,2 +1,21 @@
 class GeeksController < ApplicationController
+
+  def new
+    @geek = Geek.new
+  end
+
+  def create
+    @geek = Geek.new(geek_params)
+    if @geeek.save
+      redirect_to geek_path(@geek)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def geek_params
+    params.require(:geek).permit(:category, :name, :description, :location, :price, :active, :trusted)
+  end
 end
