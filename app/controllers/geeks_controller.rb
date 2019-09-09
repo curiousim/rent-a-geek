3e7a8a536f7db9f20338067db940f110345645ee
Kirill Menshov <kirill.menshov@gmail.com>
2019-09-09 10:24:09 +0300

    started geek controller

--- app/controllers/geeks_controller.rb
+++ app/controllers/geeks_controller.rb
@@ -1,2 +1,20 @@
 class GeeksController < ApplicationController
+
+  def index
+    Geek.all
+  end
+
+  def show
+  end
+
+
+    private
+
+    def geek_params
+      params.require(:geek).permit(:name, :photo)
+    end
+  
+    def find_geek
+      @geek = Geek.find(params[:id])
+    end
 end
