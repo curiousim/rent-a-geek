3ef4c2dd54a9ddb55df04da8c953438d1624bf04
IgorZ <igor_zlobinski@bezeqint.net>
2019-09-10 16:13:22 +0300

    Bookings added Rating

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_101426) do
+ActiveRecord::Schema.define(version: 2019_09_10_131024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_09_10_101426) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "completed", default: false
+    t.integer "rating"
     t.index ["geek_id"], name: "index_bookings_on_geek_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
