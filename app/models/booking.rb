bc51bdeae03608c0917a2abb7e0f824985f59725
Kirill Menshov <kirill.menshov@gmail.com>
2019-09-11 15:23:46 +0300

    started

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,10 +1,8 @@
 class Booking < ApplicationRecord
-  HOURS = ['One hour', 'Two hours', 'Three hours', 'Four hours', 'Five hours', 'Six hours']
   belongs_to :user
   belongs_to :geek
-  validates :date, presence: true
-  validates :time, presence: true
-  validates :duration, presence: true, inclusion: { in: HOURS }
-  validates :location, presence: true
-  validates :completed
+  #validates :date, presence: true
+  #validates :time, presence: true
+  validates :duration, presence: true
+  validates :address, presence: true
 end
