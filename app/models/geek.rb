0d355b8d1317631e7292a5e967352f6c5481c7b5
Alisa Givertz <alisagivertz@gmail.com>
2019-09-11 14:27:00 +0300

    model validations done

--- app/models/geek.rb
+++ app/models/geek.rb
@@ -4,5 +4,4 @@ class Geek < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_location?
   belongs_to :user
   has_many :bookings, dependent: :destroy
-  HOURS = ['One hour', 'Two hours', 'Three hours', 'Four hours', 'Five hours', 'Six hours']
 end
